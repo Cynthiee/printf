fe4b7d1510a1581dcba813c662d88536b8fce7cb
realJohnAdex <johnadex60@gmail.com>
2022-10-17 23:30:11 +0100

    Update _printf.c file

--- _printf.c
+++ _printf.c
@@ -11,57 +11,47 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int len = 0;
-    int i = 0;
-    char *s;
-    
-    va_start(ap, format);
-    
-    if (format)
-    {
-        while (*(format + i))
-        {
-            switch (*(format + i))
-            {
-                case '%':
-                    write(1, "", 0);
-                    i++;
-                    switch (*(format + i))
-                    {
-                        case '%':
-                            write (1, "%", 1);
-                            i++;
-                            len += 1;
-                            continue;
-                        case 'c':
-                            s = va_arg(ap, int);
-                            write(1, &s, 1);
-                            i++;
-                            len += 1;
-                            continue;
-                        case 's':
-                            s = va_arg(ap, char *);
-                            write(1, s, strlen(s));
-                            i++;
-                            len += strlen(s);
-                            continue;
-                    }
-            }
-            write(1, format + i, 1);
-            i++;
-            len += 1;
-        }
-    }
-    va_end(ap);
-    return (len);
-}
+	va_list ap;
+	int len = 0;
+	int i = 0;
+	char *s;
 
-int main() {
-    char c = 's';
-    char *s = "Nine";
-    int len = _printf("%% %c %s mmm", c, s);
-    printf("\n%d", len);
-
-    return 0;
+	va_start(ap, format);
+	if (format)
+	{
+		while (*(format + i))
+		{
+			switch (*(format + i))
+			{
+				case '%':
+					write(1, "", 0);
+					i++;
+					switch (*(format + i))
+					{
+						case '%':
+							write(1, "%", 1);
+							i++;
+							len += 1;
+							continue;
+						case 'c':
+							s = va_arg(ap, int);
+							write(1, &s, 1);
+							i++;
+							len += 1;
+							continue;
+						case 's':
+							s = va_arg(ap, char *);
+							write(1, s, strlen(s));
+							i++;
+							len += strlen(s);
+							continue;
+					}
+			}
+			write(1, format + i, 1);
+			i++;
+			len += 1;
+		}
+	}
+	va_end(ap);
+	return (len);
 }
