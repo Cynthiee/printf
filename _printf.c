ebfe511b03768fb470190439e992450b6c84ac77
realJohnAdex <johnadex60@gmail.com>
2022-10-18 13:39:18 +0100

    Add task 0 files

--- _printf.c
+++ _printf.c
@@ -1,57 +1,28 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _printf - Receives the main string and all the necessary parameters to
  * print a formated string
- * @format: A string containing all the desired character
+ * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int len = 0;
-	int i = 0;
-	char *s;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	va_start(ap, format);
-	if (format)
-	{
-		while (*(format + i))
-		{
-			switch (*(format + i))
-			{
-				case '%':
-					write(1, "", 0);
-					i++;
-					switch (*(format + i))
-					{
-						case '%':
-							write(1, "%", 1);
-							i++;
-							len += 1;
-							continue;
-						case 'c':
-							s = va_arg(ap, int);
-							write(1, &s, 1);
-							i++;
-							len += 1;
-							continue;
-						case 's':
-							s = va_arg(ap, char *);
-							write(1, s, strlen(s));
-							i++;
-							len += strlen(s);
-							continue;
-					}
-			}
-			write(1, format + i, 1);
-			i++;
-			len += 1;
-		}
-	}
-	va_end(ap);
-	return (len);
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
--- main.h
+++ main.h
@@ -1,9 +1,28 @@
 #ifndef PRINTF_FUNCTIONS_H
 #define PRINTF_FUNCTIONS_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
 
-int main(void);
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 #endif /* PRINTF_FUNCTIONS_H */
